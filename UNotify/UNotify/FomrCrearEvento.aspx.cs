f2537e198093f17721145c975fc4e773afc395a7
Dario Espinoza <darioespinoza477@gmail.com>
2023-09-11 15:12:24 -0600

    Vista | Crear Evento

---
+++ UNotify/UNotify/FomrCrearEvento.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UNotify
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PageBody.Attributes.Add("bgcolor", "1E2126");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
