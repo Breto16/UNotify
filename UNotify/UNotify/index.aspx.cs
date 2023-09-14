11e10c422a6955a7c0222b429f817547997d94c9
Breto16 <50472204+Breto16@users.noreply.github.com>
2023-09-13 20:33:42 -0600

    Update index.aspx.cs

--- UNotify/UNotify/index.aspx.cs
+++ UNotify/UNotify/index.aspx.cs
@@ -9,7 +9,7 @@ namespace UNotify
 {
     public partial class index : System.Web.UI.Page
     {   
-    {
+    
         protected void Page_Load(object sender, EventArgs e)
         {
             PageBody.Attributes.Add("bgcolor", "1E2126");
